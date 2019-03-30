aae56f35b1ee97aebb4995b42b7c322edd21319d
Elias Dolinsek <elias.dolinsek@gmail.com>
2019-03-30 16:01:54 +0100

    Finished Project

--- src/com/dolinsek/elias/randomcharsfinder/ui/MainController.java
+++ src/com/dolinsek/elias/randomcharsfinder/ui/MainController.java
@@ -1,14 +1,14 @@
 package com.dolinsek.elias.randomcharsfinder.ui;
 
 import com.dolinsek.elias.randomcharsfinder.core.RandomCharFinder;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.net.URL;
 import java.util.Calendar;
 import java.util.ResourceBundle;
@@ -38,10 +38,13 @@ public class MainController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         setupListener();
         btnStart.setOnAction(event -> {
-            lastUpdateTimestamp = 0;
-            randomCharFinder = new RandomCharFinder(getApprovedChars(), getWordsToFind(), randomCharFinderListener);
-            randomCharFinder.reset();
-            randomCharFinder.start(getThreads());
+            if (getApprovedChars().equals("") || getWordsToFind().equals("")){
+                showErrorDialogToFillOutFields();
+            } else {
+                randomCharFinder = new RandomCharFinder(getApprovedChars(), getWordsToFind(), randomCharFinderListener);
+                randomCharFinder.reset();
+                randomCharFinder.start(getThreads());
+            }
         });
 
         btnStop.setOnAction(event -> randomCharFinder.stop());
@@ -49,9 +52,12 @@ public class MainController implements Initializable {
         btnReset.setOnAction(event -> {
             randomCharFinder.reset();
         });
+
+        spnThreads.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, 4));
+        spnUpdateTime.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(100, 10000, 1000, 500));
     }
 
-    private void setupListener(){
+    private void setupListener() {
         randomCharFinderListener = new RandomCharFinder.RandomCharFinderListener() {
             @Override
             public void onStart() {
@@ -81,7 +87,7 @@ public class MainController implements Initializable {
 
             @Override
             public void onReset() {
-                txtStopTime.setText("unknown");
+                txtStartTime.setText("unknown");
                 txtRunningTime.setText("unknown");
                 txtStopTime.setText("unknown");
                 txtLoopsPerSecond.setText("unknown");
@@ -95,43 +101,59 @@ public class MainController implements Initializable {
         };
     }
 
-    private String getApprovedChars(){
-        return fldApprovedChars.getText().trim() + (cbxIncludeWhitespace.isSelected() ? " "  : "");
+    private String getApprovedChars() {
+        return fldApprovedChars.getText().trim() + (cbxIncludeWhitespace.isSelected() ? " " : "");
     }
 
-    private String getWordsToFind(){
-        return  fldWordsToFind.getText();
+    private String getWordsToFind() {
+        return fldWordsToFind.getText();
     }
 
-    private int getThreads(){
-        return 4;
+    private int getThreads() {
+        return (int) spnThreads.getValue();
     }
 
-    private void displayUpdateIfNecessary(BigInteger loops){
-        if (System.currentTimeMillis() - lastUpdateTimestamp >= getUpdateTime()){
-           txtRunningTime.setText(getTimeAsReadableString(randomCharFinder.getRunningTime()));
-           txtTotalLoops.setText(loops.toString());
-           txtLoopsPerSecond.setText(loops.divide(BigInteger.valueOf(randomCharFinder.getRunningTime() + 1)).toString());
-           lastUpdateTimestamp = System.currentTimeMillis();
+    private void displayUpdateIfNecessary(BigInteger loops) {
+        if (System.currentTimeMillis() - lastUpdateTimestamp >= getUpdateTime()) {
+            txtRunningTime.setText(getTimeAsReadableString(randomCharFinder.getRunningTime()));
+            txtTotalLoops.setText(loops.toString());
+            txtLoopsPerSecond.setText(getAverageLoopsPerSecond(loops));
+            lastUpdateTimestamp = System.currentTimeMillis();
         }
     }
 
-    private long getUpdateTime(){
-        return 1000;
+    private String getAverageLoopsPerSecond(BigInteger loops){
+        if (randomCharFinder.getRunningTime() == 0){
+            return "0";
+        } else {
+            long runningTime = randomCharFinder.getRunningTime();
+            BigInteger bigInteger = loops.divide(BigInteger.valueOf(runningTime));
+            bigInteger = bigInteger.multiply(BigInteger.valueOf(1000));
+            return bigInteger.toString();
+        }
     }
 
-    private String getDateStampAsReadableString(long timeStamp){
+    private long getUpdateTime() {
+        return (int) spnUpdateTime.getValue();
+    }
+
+    private String getDateStampAsReadableString(long timeStamp) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(timeStamp);
 
         return calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR) + " " + calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND) + "." + calendar.get(Calendar.MILLISECOND);
     }
 
-    private String getTimeAsReadableString(long timeInMillis){
-        int seconds = (int) (timeInMillis / 1000) % 60 ;
-        int minutes = (int) ((timeInMillis / (1000*60)) % 60);
-        int hours   = (int) ((timeInMillis / (1000*60*60)) % 24);
+    private String getTimeAsReadableString(long timeInMillis) {
+        int seconds = (int) (timeInMillis / 1000) % 60;
+        int minutes = (int) ((timeInMillis / (1000 * 60)) % 60);
+        int hours = (int) ((timeInMillis / (1000 * 60 * 60)) % 24);
 
-        return hours + ":" + minutes + ":" + seconds + ".";
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+    private void showErrorDialogToFillOutFields(){
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Please fill out all fields", ButtonType.OK);
+        alert.show();
     }
 }
